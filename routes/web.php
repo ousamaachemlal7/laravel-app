c8eb2c38b63121b3b83991336b401785c9e6cb60
Oussama Achemlal <91486759+ousamaachemlal7@users.noreply.github.com>
2022-09-12 18:16:14 +0100

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,22 @@ Route::get('/about', function () {
     return view('about');
 });
 
+/*
 Route::get('/store', function () {
     $filter = request('style');
     if(isset($filter)){
-        return 'this page is viewing <span style="color:red">'.$filter.'</span>';
+        return 'this page is viewing <span style="color:red">'.strip_tags($filter).'</span>';
     }
     return 'this page is viewing <span style="color:red">  all products </span>';
+});
+*/
+
+Route::get('/store/{catergory?}/{item?}', function ($category=null,$item=null) {
+    if(isset($category)){
+        if(isset($item)){
+            return '<h1>'.$item.'</h1>';
+        }
+        return '<h1>'.$category.'</h1>';
+    }
+    return '<h1> STORE </h1>';
 });
