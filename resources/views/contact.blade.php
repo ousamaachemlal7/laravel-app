a3afdedc0011ed053deeac11f58f7b4bac09469b
Oussama Achemlal <91486759+ousamaachemlal7@users.noreply.github.com>
2022-09-14 19:07:23 +0100

    sixth  commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -411,12 +411,12 @@
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1>ABOUT ME</h1>
+                <h1>CONTACT ME</h1>
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div>
-                    THIS IS THE ABOUT PAGE
+                    THIS IS THE CONTACT PAGE
                 </div>
             </div>
 
